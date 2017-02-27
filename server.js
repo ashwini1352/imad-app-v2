0eab8f292dbe0c9abae47f4179079a769ffc78d0
Ashwani Kumar <ashwini13522@gmail.com>
2017-02-27 18:04:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,6 +131,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     //articleName ==  articleOne
+    //articles = articleName[articles]
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
